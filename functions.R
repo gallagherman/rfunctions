8657074fc0ea3dfc5c8034018168edd97a92fd43
Michael Gallagher <gallagherman@gmail.com>
2022-12-16 14:37:06 -0500

    Added titr function.

--- functions.R
+++ functions.R
@@ -1,4 +1,10 @@
-testfun <- function(){
-  message('The world is a vampire.')
-}
+##### Collection of custom functions
 
+# Create serial-dilution titration curve
+titr <- function(top,fold,n){
+  out <- c(top)
+  for(i in 2:n){
+    out <- c(out, out[i-1]/fold)
+  }
+  return(out)
+}
